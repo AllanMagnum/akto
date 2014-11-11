7eefb4cf5b9edc0f72025af401a1563653348f21
AllanMagnum <AllanMagnum@AllanMagnum-PC>
2014-11-11 17:54:12 -0400

    conexao

--- cau/util/Conexao.php
+++ cau/util/Conexao.php
@@ -5,8 +5,8 @@ class Conexao
 
 	function getConnection()
 	{
-// 		$this->con = mysqli_connect("localhost:3306","root","magnum", "akto_cau");
-		$this->con = mysqli_connect("localhost","root","root", "akto_cau");
+ 		$this->con = mysqli_connect("localhost:3306","root","magnum", "akto_cau");
+//		$this->con = mysqli_connect("localhost","root","root", "akto_cau");
 		if (mysqli_connect_error()) {
 			echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		}
