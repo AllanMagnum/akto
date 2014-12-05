ac17d619ecf86a7abec0cfc54c72fdb9c9f0c08b
AllanMagnum <AllanMagnum@AllanMagnum-PC>
2014-12-05 18:30:58 -0400

    atualizacao do metodo atualizar da classe PessoaFisicaDAO

--- cau/model/dao/PessoaFisicaDAO.php
+++ cau/model/dao/PessoaFisicaDAO.php
@@ -24,7 +24,7 @@ class PessoaFisicaDAO{
 	
 	function atualizar($o_pessoaFisica){
 		$this->sql = "update pessoafisica set nome= '" . $o_pessoaFisica->getNome() . "', cpf= '" . $o_pessoaFisica->getCpf() . "', datanascimento=  '" . $o_pessoaFisica->getDataNascimento() . "', " . 
-		              "estadocivil = '" . $o_pessoaFisica->getEnumEstadoCivil() . "', sexo= '" . $o_pessoaFisica->EnumgetSexo() . "', nomePai= '" . $o_pessoaFisica->getNomePai() . "', " .
+		              "estadocivil = '" . $o_pessoaFisica->getEnumEstadoCivil() . "', sexo= '" . $o_pessoaFisica->getEnumSexo() . "', nomePai= '" . $o_pessoaFisica->getNomePai() . "', " .
 		              "nomeMae = '" . $o_pessoaFisica->getNomeMae() . "', cor= '" . $o_pessoaFisica->getCor() . "', naturalidade= '" . $o_pessoaFisica->getNaturalidade() . "', " .
 		              "nacionalidade = '" . $o_pessoaFisica->getNacionalidade() . "', dataAtualizacao= '" . $o_pessoaFisica->getDataAtualizacao() . "'" .
 		             " where id='" . $o_pessoaFisica->getId() ."'" ;
